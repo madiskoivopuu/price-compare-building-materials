053ff1dec3a27c8460d389aa90636ce3b3d7aba8
jaannigul <jaannigul@gmail.com>
2024-10-14 19:56:05 +0300

    search header

--- frontend/src/components/Hero.js
+++ frontend/src/components/Hero.js
@@ -1,62 +1,84 @@
 import React from 'react'
-import { useState, useEffect } from 'react'
+import {useState, useEffect} from 'react'
 import SearchResult from './SearchResult'
+import SearchHeader from './SearchHeader';
 
 function Hero() {
-    const [items, setItems] = useState({})
+    const [items, setItems] = useState({products: []})
     const [q, setQ] = useState('')
     const [inputValue, setInputValue] = useState('')
+    const [currentPage, setCurrentPage] = useState(1);
+    const productsPerPage = 25; //currently a constant
 
     useEffect(() => {
-        fetch(`http://localhost:8080/request/search?keyword=${q}`)
-        .then((res) => res.json())
-        .then((res) => setItems(res))
-    }, [q])
-    
+        if (q) {
+            fetch(`http://localhost:8080/request/search?keyword=${q}`)
+                .then((res) => res.json())
+                .then((res) => {
+                    const sortedProducts = res.products?.sort((a, b) => parseFloat(a.price) - parseFloat(b.price));
+                    setItems({...res, products: sortedProducts});
+                    setCurrentPage(1);
+                })
+                .catch((err) => console.error("Fetch error:", err));
+        }
+    }, [q]);
+
+    const indexOfLastProduct = currentPage * productsPerPage;
+    const indexOfFirstProduct = indexOfLastProduct - productsPerPage;
+    const currentProducts = items.products?.slice(indexOfFirstProduct, indexOfLastProduct);
+    console.log(currentProducts)
+
 
     return (
-    <div className='w-full md:w-3/4 h-max p-10'>
-        <form onSubmit={(e) => {
-            e.preventDefault()
-            setQ(inputValue)
-        }}
-            className='flex border-2 border-gray-200 rounded mb-4'
-        >
-            <input 
-            className='w-full h-12 pl-2' 
-            type='text' 
-            placeholder='Otsi toodet'
-            onChange={(e) => setInputValue(e.target.value)}
-            > 
-            </input>
-            <button type='submit' className='mx-2'>
-                <svg width="20px" height="20px" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-                    <path d="M15.7955 15.8111L21 21M18 10.5C18 14.6421 14.6421 18 10.5 18C6.35786 18 3 14.6421 3 10.5C3 6.35786 6.35786 3 10.5 3C14.6421 3 18 6.35786 18 10.5Z" stroke="#000000" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"/>
-                </svg>
-            </button>
-        </form>
+        <div className='w-full md:w-3/4 h-max p-10'>
+            <form onSubmit={(e) => {
+                e.preventDefault()
+                setQ(inputValue)
+            }}
+                  className='flex border-2 border-gray-200 rounded mb-4'
+            >
+                <input
+                    className='w-full h-12 pl-2'
+                    type='text'
+                    placeholder='Otsi toodet'
+                    onChange={(e) => setInputValue(e.target.value)}
+                >
+                </input>
+                <button type='submit' className='mx-2'>
+                    <svg width="20px" height="20px" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                        <path
+                            d="M15.7955 15.8111L21 21M18 10.5C18 14.6421 14.6421 18 10.5 18C6.35786 18 3 14.6421 3 10.5C3 6.35786 6.35786 3 10.5 3C14.6421 3 18 6.35786 18 10.5Z"
+                            stroke="#000000" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"/>
+                    </svg>
+                </button>
+            </form>
 
-        
+            <SearchHeader
+                totalProducts={items.products.length}
+                currentPage={currentPage}
+                setCurrentPage={setCurrentPage}
+                productsPerPage={productsPerPage}
+            />
 
-        <div className='w-full h-max'>
-            <ul className='flex flex-col gap-4'>
-                {items.products?.map((item, index) => 
-                    items.products.length > 0 
-                    ?
-                    (<li key={index}>
-                            <SearchResult 
-                                name={item.name} 
-                                linkToPicture={item.linkToPicture}
-                                linkToProduct={item.linkToProduct}
-                                price={item.price}
-                            />
-                    </li>)
-                    : 
-                    <li></li>
-                )}
-            </ul>
+            <div className='w-full h-max'>
+                <ul className='flex flex-col gap-4'>
+                    {currentProducts?.map((item, index) =>
+                        items.products.length > 0
+                            ?
+                            (<li key={index}>
+                                <SearchResult
+                                    name={item.name}
+                                    linkToPicture={item.linkToPicture}
+                                    linkToProduct={item.linkToProduct}
+                                    price={item.price}
+                                />
+                            </li>)
+                            :
+                            <li></li>
+                    )}
+                </ul>
+            </div>
         </div>
-    </div>
     )
 }
 
--- frontend/src/components/SearchHeader.js
+++ frontend/src/components/SearchHeader.js
@@ -1,9 +1,43 @@
 import React from 'react'
 
-function SearchHeader() {
+function SearchHeader({ totalProducts, currentPage, setCurrentPage, productsPerPage}) {
+
+  const totalPages = Math.ceil(totalProducts / productsPerPage);
+  const handlePageChange = (pageNumber) => {
+    setCurrentPage(pageNumber);
+  }
   return (
-    <div>SearchHeader</div>
-  )
+      <div className="flex justify-between items-center p-4 border-b">
+        <div className="text-gray-700">
+          <p>Leitud {totalProducts} toodet</p>
+        </div>
+        <div className="flex items-center gap-2 justify-center flex-grow">
+          <button
+              onClick={() => handlePageChange(currentPage - 1)}
+              disabled={currentPage === 1}
+              className="p-2 border rounded"
+          >
+            {'<'}
+          </button>
+          {Array.from({ length: totalPages }, (_, i) => i + 1).map((page) => (
+              <button
+                  key={page}
+                  onClick={() => handlePageChange(page)}
+                  className={`w-8 h-8 p-2 border rounded-full flex items-center justify-center ${page === currentPage ? 'bg-gray-300' : ''}`}
+              >
+                {page}
+              </button>
+          ))}
+          <button
+              onClick={() => handlePageChange(currentPage + 1)}
+              disabled={currentPage === totalPages}
+              className="p-2 border rounded"
+          >
+            {'>'}
+          </button>
+        </div>
+      </div>
+  );
 }
 
 export default SearchHeader
