0812ecbb8acd22266985dbd7d47087ebf795fb94
jaannigul <jaannigul@gmail.com>
2024-10-24 17:22:05 +0300

    aws test environment deployment, pipeline docker build and push

--- backend/src/main/java/priceCompare/backend/controller/WebConfig.java
+++ backend/src/main/java/priceCompare/backend/controller/WebConfig.java
@@ -14,7 +14,7 @@ public class WebConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:3000", "https://ematerjal.eu")  // Allow both localhost and your production domain
+                        .allowedOrigins("http://localhost:3000", "https://ematerjal.eu, http://16.16.186.149:3000")  // Allow both localhost and your production domain
                         .allowedMethods("GET", "POST", "PUT", "DELETE")
                         .allowedHeaders("*")
                         .allowCredentials(true);
--- frontend/src/components/Hero.js
+++ frontend/src/components/Hero.js
@@ -13,7 +13,7 @@ function Hero() {
 
     useEffect(() => {
         if (q) {
-            fetch(`http://localhost:8080/request/search?keyword=${q}`)
+            fetch(`http://16.16.186.149:8080/request/search?keyword=${q}`)
                 .then(async (response) => {
                     const decoder = new TextDecoder('utf-8');
                     const decodedResponse = decoder.decode(await response.arrayBuffer());
