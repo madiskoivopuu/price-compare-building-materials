893cad134a3b647641eb867b141a5a4d122d277a
Madis Kõivopuu <32608971+madiskoivopuu@users.noreply.github.com>
2024-11-20 18:44:29 +0200

    #76 | Update proxy url

--- backend/src/main/java/priceCompare/backend/BackendApplication.java
+++ backend/src/main/java/priceCompare/backend/BackendApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class BackendApplication {
-	public static final String IMAGE_LOADING_PROXY_URL = "http://localhost:8080/request/proxy-img-req";
+	public static final String IMAGE_LOADING_PROXY_URL = "http://16.16.186.149:8080/request/proxy-img-req";
 
 	public static void main(String[] args) {
 		SpringApplication.run(BackendApplication.class, args);
